895e374d313f4840f34acd2e6e3687e823806ee5
isarmadfarooq <i.sarmadfarooq@gmail.com>
2024-10-05 14:28:45 +0500

    Complete the Front End setup full react js

---
+++ investment-ui/src/redux/reducers/rootReducer.js
@@ -0,0 +1,23 @@
+// src/redux/reducers/rootReducer.js
+
+import { combineReducers } from 'redux';
+
+// Import your individual reducers here
+
+// import someReducer from './someReducer';
+
+
+
+const rootReducer = combineReducers({
+
+  /// Add your reducers here
+
+    // some: someReducer,
+
+    });
+
+
+
+    export default rootReducer;
+
+    
